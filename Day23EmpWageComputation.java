49e45db1c24c37c61e2a5e37818466c20a78344a
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 22:05:45 +0530

    Emp Wage for WorkingDays

--- Day23EmpWageComputation.java
+++ Day23EmpWageComputation.java
@@ -1,30 +1,41 @@
 public class Day23EmpWageComputation{
 
-	public static final int isPartTime = 1;
-	public static final int isFullTime = 2;
-	public static final int empRatePerHr=20;
+	public static final int isFullTime = 1;
+	public static final int isPartTime = 2;
 
 	public static void main(String [] args){
 
-
+		int empRatePerHr = 20;
 		int empHrs = 0;
-		int empWage = 0;
+		int salary = 0;
+		int empWorkingDays = 20;
+		int totalSal = 0;
 
-		int empCheck = (int) Math.floor(Math.random()*10) % 3;
+		int empCheck = (int) (Math.floor(Math.random()* 3));
 
 		switch(empCheck){
 
-			case isPartTime :
-				empHrs = 4;
-				break;
 			case isFullTime :
+				System.out.println("Full Time Salary");
 				empHrs = 8;
 				break;
-
+			case isPartTime :
+				System.out.println("Half Time Salary");
+				empHrs = 4;
+				break;
 			default :
+				System.out.println("When Employee is Absent");
 				empHrs = 0;
 		}
-		empWage = empHrs * empRatePerHr;
-		System.out.println("Emp Wage "+ empWage);
+
+		salary = empHrs* empRatePerHr;
+		System.out.println("Calculate Salary = "+salary);
+
+		for (int i = 1;i <= empWorkingDays; i++ ){
+
+			totalSal = salary * empWorkingDays;
+		}
+
+		System.out.println("Total Salary = "+totalSal);
 	}
 }
