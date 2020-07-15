f80d61d12bafb190dcffdde1f7834ee5e4a47bd3
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 22:11:47 +0530

    empWage for Max hrs And Working Days

--- Day23EmpWageComputation.java
+++ Day23EmpWageComputation.java
@@ -1,41 +1,40 @@
 public class Day23EmpWageComputation{
 
-	public static final int isFullTime = 1;
-	public static final int isPartTime = 2;
+	public static final int isPartTime = 1;
+	public static final int isFullTime = 2;
+	public static final int empRatePerHr=20;
+	public static final int numOfWorkingDays=3;
+	public static final int maxHrsInMonth=10;
 
 	public static void main(String [] args){
 
-		int empRatePerHr = 20;
+
 		int empHrs = 0;
-		int salary = 0;
-		int empWorkingDays = 20;
-		int totalSal = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 
-		int empCheck = (int) (Math.floor(Math.random()* 3));
+		while (totalEmpHrs <= maxHrsInMonth && totalWorkingDays < numOfWorkingDays){
+			totalWorkingDays++;
 
-		switch(empCheck){
+			int empCheck = (int) Math.floor(Math.random()*10) % 3;
 
-			case isFullTime :
-				System.out.println("Full Time Salary");
-				empHrs = 8;
-				break;
-			case isPartTime :
-				System.out.println("Half Time Salary");
-				empHrs = 4;
-				break;
-			default :
-				System.out.println("When Employee is Absent");
-				empHrs = 0;
+			switch(empCheck){
+
+				case isPartTime :
+					empHrs = 4;
+					break;
+				case isFullTime :
+					empHrs = 8;
+					break;
+
+				default :
+					empHrs = 0;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Day : "+totalWorkingDays + " Emp Hrs : "+empHrs);
 		}
 
-		salary = empHrs* empRatePerHr;
-		System.out.println("Calculate Salary = "+salary);
-
-		for (int i = 1;i <= empWorkingDays; i++ ){
-
-			totalSal = salary * empWorkingDays;
-		}
-
-		System.out.println("Total Salary = "+totalSal);
+		int totalEmpWage = totalEmpHrs * empRatePerHr;
+		System.out.println("Total Emp Wage "+ totalEmpWage);
 	}
-}
+}	
